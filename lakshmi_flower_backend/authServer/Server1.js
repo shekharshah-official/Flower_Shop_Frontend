bab999e9a4d8efc78230e779536c2d10cd8fe164
r s <r@ruddra.ruddra>
2024-10-29 15:48:02 +0530

    backend seperated

--- lakshmi_flower_backend/authServer/Server1.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app');
-
-// Start the server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- lakshmi_flower_backend/authServer/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Route to handle login requests
-router.post('/', authController.login);
-
-module.exports = router;
